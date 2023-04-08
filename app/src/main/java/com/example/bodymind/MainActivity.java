8a6981bb0176ddc2ca8dfe81fd78eb560ded999b
aditya <121624446+Adityasah2004@users.noreply.github.com>
2023-04-08 13:05:44 +0530

    Add files via upload

--- app/src/main/java/com/example/bodymind/MainActivity.java
+++ app/src/main/java/com/example/bodymind/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.bodymind;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.motion.widget.MotionLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,17 +13,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivities(new Intent[]{new Intent(MainActivity.this, MainActivity2.class)});
+                startActivity(new Intent(MainActivity.this,MainActivity2.class));
                 finish();
             }
         },3000);
+
 
     }
 }
